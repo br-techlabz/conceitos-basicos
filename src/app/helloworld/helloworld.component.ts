ee0236341937336f73c0d58f317a82498c2ca42c
Willian <br.techlabz@proton.me>
2026-01-03 18:40:45 +0000

    Aula 11 - criando componentes atraves da CLI

--- src/app/helloworld/helloworld.component.ts
+++ src/app/helloworld/helloworld.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'hello-world',
-    template: '<h1> Hello Worl Guys! </h1>'
+    template: '<h1> Hello World Guys! </h1>'
 })
 export class HelloWorldComponent{
 
