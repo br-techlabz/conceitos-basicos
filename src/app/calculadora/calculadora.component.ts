594dda8bed8153c04926348f6e23ad47f785ca8c
Willian <br.techlabz@proton.me>
2026-01-04 11:34:03 +0000

    Aua 15: submetando um formulario com submit Aula 16: tranalhando com condicional IF

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,16 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [ FormsModule ],
+  imports: [ FormsModule, CommonModule ],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
 
 export class CalculadoraComponent {
   
-  numero1: number = 15;
+  numero1: number = 0;
   numero2: number = 0;
   resultado: number = 0;
 
