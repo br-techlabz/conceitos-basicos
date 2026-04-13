6be43dbdfd324249a238278be0b62abc9e621dd5
willian <br.techlabz@proton.me>
2026-04-13 07:58:44 -0300

    meu primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MinhapaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
