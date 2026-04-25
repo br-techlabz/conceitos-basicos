a261738a9533a67773a8568d397827e4d7814442
willian <br.techlabz@proton.me>
2026-04-25 18:57:06 -0300

    criação do componente lista-compras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MinhapaginaComponent } from './minhapagina/minhapagina.component';
 import {CalculadoraComponent} from './calculadora/calculadora.component';
+import { ListaComprasComponent} from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MinhapaginaComponent, CalculadoraComponent],
+  imports: [RouterOutlet, MinhapaginaComponent, ListaComprasComponent, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
